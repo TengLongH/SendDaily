e322fb58db4833f38d079e2dea3efdf8227bd880
TengLongH <TengLongH@126.com>
2016-06-06 19:27:41 +0800

    auto loop run

--- src/main/Go.java
+++ src/main/Go.java
@@ -2,7 +2,10 @@ package main;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.SimpleFormatter;
+
 import beans.PersonDaily;
 import database.Database;
 import email.Send;
@@ -12,14 +15,43 @@ public class Go {
 
 	public static void main(String[] args) {
 		
-		Calendar calendar = Calendar.getInstance();
-		SimpleDateFormat format = new SimpleDateFormat("YY.MM.dd");
-		String subject = "综合组" + format.format(calendar.getTime()) + "日报";
+		boolean sendTime = false;
+		boolean sendAlready = false;
+		Go go = new Go();
+		SimpleDateFormat format = new SimpleDateFormat("YY.MM.ddE");
+
+		while( true ){
+			sendTime = go.sendTime();
+			if( sendTime ){
+				if( !sendAlready){
+					String subject = "综合组" + format.format(new Date() ) + "日报";
+					sendAlready = go.run(subject);
+				}
+			}else{
+				sendAlready = false;
+			}
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public boolean sendTime( ){
+		Date now = new Date();
+		if( now.getDay() == 1 || now.getDay() == 2 ){
+			Date limit = new Date( now.getYear(), now.getMonth(), now.getDate(), 21, 30 );
+			if( now.compareTo(limit) > 0  )return true;
+			return false;
+		}
+		return false;
+	}
+	public boolean run( String subject ){
 		
 		Internet net = new Internet();
 		Database database = new Database();
 		Send send = new Send();
-		
 		try{
 			net.Connect();
 			send.setSubject(subject);
@@ -36,9 +68,10 @@ public class Go {
 			send.getRecivers().add("hongtenglong@sohu.com");
 			
 			send.send();
+			return true;
 		}catch( Exception e ){
 			e.printStackTrace();
 		}
+		return false;
 	}
-
 }
